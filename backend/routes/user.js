f95058ead12c5759ea679168d102f8143267ebd5
dipankar2002 <dipankarganguly15@gmail.com>
2024-10-22 23:30:42 +0530

    asfasf

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,6 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
-const { User, Course } = require('../db/index');
 const jwt = require('jsonwebtoken');
 const {jwtPass} = require('../config');
 
