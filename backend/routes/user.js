ffd4c17554328194f8a354e7d49453b6337accb4
dipankar2002 <dipankarganguly15@gmail.com>
2024-10-23 13:37:03 +0530

    user route file code

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,8 @@
 const { Router } = require("express");
 const router = Router();
-const userMiddleware = require("../middleware/user");
 const jwt = require('jsonwebtoken');
-const {jwtPass} = require('../config');
+const SECRET_KEY = require('../impDocs/jwt_key');
+const { userDb } = require("../db/accountDB");
 
 
 router.post('/seeTodo', async (req, res) => {
@@ -14,11 +14,43 @@ router.post('/createTodo', async (req, res) => {
 });
 
 router.post('/login', async (req, res) => {
-    
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const findUser = await userDb.findOne({
+    email: email,
+    password: password
+  })
+
+  if(findUser) {
+    const token = jwt.sign({ email: email },SECRET_KEY);
+    res.status(200).json({ jwt: token });
+    return;
+  }
+  res.json({ mes: "user not found create account" });
 });
 
 router.post('/signup', async (req, res) => {
-    
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
+  
+  
+  const findUser = await userDb.findOne({
+    email: email,
+    password: password
+  })
+
+  if(findUser) {
+    res.json({mes: "user found create a new one"});
+    return;
+  }
+  await userDb.create({
+    name: name,
+    email: email,
+    password: password
+  })
+  res.json({mes: "user account created"});
 });
 
 
