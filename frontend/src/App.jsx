fb31e600b0a7d2f7ec7bce1886dd7b782377bc4d
dipankar2002 <dipankarganguly15@gmail.com>
2024-10-24 12:36:49 +0530

    delete app.css import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import SignUpForm from "./components/SignUp";
 import LogInForm from "./components/LogIn";
-import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 
