4a760cd50a81ce2c59d60fbbd1df79c5508df374
dipankar2002 <dipankarganguly15@gmail.com>
2024-10-25 13:55:05 +0530

    adding phone server host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Allows access from the network
+    port: 3000       // Optional: set a custom port
+  }
 })
+
