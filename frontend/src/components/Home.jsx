9f12d9bb26f9801cf5c9582283dbb08733b16a69
dipankar2002 <dipankarganguly15@gmail.com>
2024-10-25 22:15:05 +0530

    done left section of the home page

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,57 +1,63 @@
 import React from "react";
 import { useState } from "react";
 
-function Home() {
-  /**
-    list er use state ta baniyechi karon ota user define korbe to tui oibhbae array t add kore show korbi,
-    r day ta calendar ache bole oita dekhe task manage korar jonno     */
-  const [Lists, setLists] = useState([]);
-  const [Day, setDay] = useState("Today");
+export default function Home() {
   return (
     <>
-      <div className="flex justify-between ">
-        <div className="basis-1/4 rounded-2xl bg-slate-900 opacity-60  text-left p-3 ">
-          <h1 className=" left-0 text-white text-4xl pb-4 ">Menu</h1>
-          <div className="mt-5 ">
-            <label htmlFor="tasks" className="m-1 text-3xl text-white ">
-              Tasks
-            </label>
-            <div id="tasks" className=" bg-slate-50 h-40 rounded-lg ">
-              <ul className="p-3 ">
-                <li>TODAY</li>
-                <li>UPCOMING</li>
-                <input type="date" />
-              </ul>
-            </div>
-          </div>
-          <div className="mt-5 ">
-            <label htmlFor="lists" className="m-1 text-3xl text-white">
-              Lists
-            </label>
-            <div id="lists" className=" bg-white h-40 rounded-lg ">
-              <ul className="p-3">
-                {/**user input debe to link ta use korenis  */}
-                <li>{Lists[1]}</li>
-                <li>{Lists[2]}</li>
-                <li>{Lists[3]}</li>
-              </ul>
-            </div>
-          </div>
-          <div className="m-5 flex justify-evenly ">
-            {/**button gulo sign out sign in dekhe toke manage korte hobe */}
-            <button className="bg-slate-50 rounded-lg p-2">Sign Out</button>
-            <button className="bg-slate-50 rounded-lg p-2">Settings</button>
-          </div>
-        </div>
-        <div className=" basis-3/4 flex-wrap text-left ml-7 ">
-          <div className="flex justify-between">
-            <h1 className="text-4xl mb-2">{Day}</h1>
-          </div>
-          <div className="h-[35rem] bg-slate-900 opacity-60 rounded-3xl"></div>
-        </div>
-      </div>
+      <LeftSec />
+      {/* <RightSec /> */}
     </>
   );
 }
 
-export default Home;
+function LeftSec() {
+  return <div className="bg-gray-500 bg-opacity-30 w-[20%] py-5 m-5  rounded-[20px]">
+    <Top />
+    <MidCards title={"TASKS"} innerText={{l1:"Today",l2:"Upcoming",l3:"Calender"}}/>
+    <MidCards title={"LISTS"} innerText={{l1:"Personal",l2:"Workout",l3:"Home"}} inputDisabled={"disabled"}/>
+    <BottomBtns />
+  </div>
+}
+
+function Top() {
+  return <div className="grid">
+    <div className="flex justify-between w-[90%] mx-auto">
+      <h2 className="text-white my-4 font-bold text-2xl tracking-widest">MENU</h2>
+      <button className="text-white font-bold text-[200%] tracking-widest">=</button>
+    </div>
+    <input className="bg-white bg-opacity-50 rounded-[20px] mb-4 px-5 pb-1.3 w-[90%] h-[35px] mx-auto text-white placeholder-black text-[120%] font-medium"
+      type="text" 
+      placeholder="SEARCH"/>
+  </div>
+}
+
+function MidCards({title, innerText, inputDisabled}) {
+  return <div className=" my-4 w-[95%] mx-auto">
+    <p className="ml-4 text-[120%] text-white font-bold">{title}</p>
+    <MidInfoCard innerText={innerText} inputDisabled={inputDisabled}/>
+  </div>
+}
+
+function MidInfoCard({innerText, inputDisabled}) {
+  return <div className="bg-white rounded-[20px] py-2 my-2">
+    <div className="flex justify-between w-[80%] mx-auto my-2">
+      <button className="text-[120%] font-medium">&#8658; {innerText.l1}</button>
+    </div>
+    <div className="flex justify-between w-[80%] mx-auto my-2">
+      <button className="text-[120%] font-medium">&#8658; {innerText.l2}</button>
+    </div>
+    <div className="flex justify-between w-[80%] mx-auto my-2">
+      <button className="text-[120%] font-medium">&#8658; {innerText.l3}</button>
+      <input className="text-transparent cursor-pointer w-[40%]"
+        type="date"
+        placeholder=""/>
+    </div>
+  </div>
+}
+
+function BottomBtns() {
+  return <div className="flex justify-around my-2 mx-2">
+    <button className="bg-white hover:bg-blue-300 focus:ring-4 focus:outline-none focus:ring-primary-300 font-bold rounded-[10px] text-[80%] px-4 py-2 focus:ring-blue-800 text-black">Log Out</button>
+    <button className="bg-white hover:bg-blue-300 focus:ring-4 focus:outline-none focus:ring-primary-300 font-bold rounded-[10px] text-[80%] px-4 py-2 focus:ring-blue-800 text-black">Setting</button>
+  </div>
+}
