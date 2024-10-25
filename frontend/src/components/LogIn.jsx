dcef7e467b5ff7b57594e4f752637c3ba6d8b80e
dipankar2002 <dipankarganguly15@gmail.com>
2024-10-25 22:13:43 +0530

    complete login page style

--- frontend/src/components/LogIn.jsx
+++ frontend/src/components/LogIn.jsx
@@ -2,46 +2,49 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function LogInForm() {
+  function loginBtn(e) {
+    e.preventDefault();
+  }
   return (
-    <form>
-      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0 opacity-70">
-        <div className="w-full bg-white rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0">
-          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <p className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
-              Logn In
+    <form onSubmit={loginBtn}>
+      <div className="flex h-screen justify-center items-center my-50%">
+        <div className="w-[80%] sm:w-[400px] md:w-[500px] lg:w-[700px] bg-gray-500 bg-opacity-30 rounded-[20px]  ">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8 grid grid-cols-1">
+            <p className="text-[160%] md:text-[200%] lg:text-[250%] text-center mb-2 font-bold leading-tight tracking-tight text-white">
+                LogIn
             </p>
-            <div>
-              <label className="block mb-2 text-sm font-medium text-gray-900">
-                Your username
+            <div className="flex justify-around">
+              <label className="block my-2 mx-2 text-[140%] md:text-[170%] lg:text-[200%] font-medium text-white">
+                Email
               </label>
               <input
-                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
-                id="username"
-                type="text"
+                className="bg-gray-50 border border-gray-300 text-white sm:text-sm rounded-[10px] block w-[50%] sm:w-[200px] md:w-[300px] lg:w-[460px] xl:w-[480px] p-2.5"
+                id="password"
+                type="email"
               />
             </div>
-            <div>
-              <label className="block mb-2 text-sm font-medium text-gray-900">
+            <div className="flex justify-around">
+              <label className="block my-2 mx-2 text-[140%] md:text-[170%] lg:text-[200%] font-medium text-white">
                 Password
               </label>
               <input
-                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
-                id="password"
+                className="bg-gray-50 border border-gray-300 text-white sm:text-sm rounded-[10px] block w-[50%] sm:w-[200px] md:w-[300px] lg:w-[460px] xl:w-[480px] p-2.5"
+                id="confirmPassword"
                 type="password"
               />
             </div>
 
             <button
-              className="w-full bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center  focus:ring-blue-800 text-white"
+              className="bg-blue-700 hover:bg-blue-300 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-[10px] text-[100%] md:text-[140%] px-10 py-2.5 m-auto  focus:ring-blue-800 text-white"
               type="submit"
             >
-              Log In
+              Login
             </button>
-            <p className="mt-6 flex justify-center font-sans text-sm font-light leading-normal text-inherit antialiased">
+            <p className=" text-white md:text-[20px] flex justify-center font-sans text-sm font-light leading-normal text-inherit antialiased">
               Dont have an account?
               <Link
                 to="/signup"
-                className="ml-1 block font-sans text-sm font-bold leading-normal text-cyan-500 antialiased"
+                className="ml-2 md:text-[20px] block font-sans text-sm font-bold leading-normal text-cyan-500 antialiased"
               >
                 Sign up
               </Link>
